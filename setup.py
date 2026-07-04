344f0e8952ca7240e1b1eeb9ede46e45414a1b3e
D4RKNE52 <darkness.scprp@gmail.com>
2026-07-04 19:44:27 +0200

    Ver 1.1.1 | Changed some minor stuff

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kvstore-raft",
-    version="1.1.0",
+    version="1.1.1",
     description="Distributed Key-Value Store with Raft Consensus",
     author="S0meR4nd0mGuy",
     packages=find_packages(),
